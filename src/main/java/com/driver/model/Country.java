b4c18d48b72c947c21778378ba9db9ea59e1b4e1
MushfiqueAlam <mushfique.037@gmail.com>
2025-04-09 23:18:41 +0530

    changes on country model

--- src/main/java/com/driver/model/Country.java
+++ src/main/java/com/driver/model/Country.java
@@ -7,6 +7,7 @@ public class Country {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Enumerated(EnumType.STRING)
     private CountryName countryName;
     private String countryCode;
 
